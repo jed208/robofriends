952c987b3dceb8346d89dd39fee597032d71af58
jed208 <jed208@protonamil.com>
2020-11-23 15:22:50 -0700

    changes made for React Hooks

--- src/containers/old/App-old-2.js
+++ src/containers/old/App-old-2.js
@@ -1,3 +1,11 @@
+// Updated: Nov 23, 2020
+// Updated code for React Hooks to clean things up
+// Removes constructor for setting state and componentDidMount lifecycle check
+// Uses useState and useEffect instead
+
+// https://reactjs.org/docs/react-component.html
+// https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
+
 import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
@@ -6,8 +14,23 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 function App() {
+	// constructor() {
+	// 	super()
+	// 	this.state = {
+	// 		robots: [],
+	// 		searchfield: ''
+	// 	}
+	// 	console.log('constructor');
+	// }
 	const [robots, setRobots] = useState([])
 	const [searchfield, setSearchfield] = useState('')
+
+	// componentDidMount() {
+	// 	fetch('https://jsonplaceholder.typicode.com/users')
+	// 		.then(response => response.json()) // .json converts the response to something that can be used in javascript
+	// 		.then(users => this.setState( { robots: users }));
+	// 	console.log('componentDidMount');
+	// }
 
 	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
